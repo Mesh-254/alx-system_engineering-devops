42378bcde68128d09c0764e9f1086ef5cf8c3edc
Mesh-254 <meshack1995@gmail.com>
2022-04-26 17:33:01 +0300

    function that queries the Reddit API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,10 +5,12 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    user = {"User-Agent": "Ahmed_belhaj"}
+    user = {"User-Agent": "Meshack"}
     request = requests.get("https://www.reddit.com/r/{}/about.json"
                            .format(subreddit), headers=user)
     if request.status_code == 200:
         return request.json().get("data").get("subscribers")
     else:
         return 0
+
+        
